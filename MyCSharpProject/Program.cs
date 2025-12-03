beabe3e1182e2e1ad2acc27c59db2cdbf9460b3a
Максим Поліщук <polishchuk.maksim2308@gmail.com>
2025-12-03 12:15:45 +0200

    Add initial project files and dependencies for the OOP project
    - Created  with target framework set to net8.0
    - Added necessary package references for Microsoft.Extensions libraries
    - Introduced a project reference to itself, which caused a cycle error (NU1108)
    - Generated  to log package resolution issues

---
+++ MyCSharpProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCSharpProject
+{
+    public class Library
+    {
+        private string name;
+        private string address;
+
+        private List<string> books;
+
+        public int BooksCount => books.Count;
+
+        public Library(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+            books = new List<string>();
+        }
+
+        public void AddBook(string bookTitle)
+        {
+            books.Add(bookTitle);
+            Console.WriteLine($"Книга \"{bookTitle}\" додана до бібліотеки.");
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Бібліотека: {name}, Адреса: {address}, Книг: {BooksCount}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Library
+{
+    private string name;
+    private string address;
+    private List<string> books;
+
+    public int BooksCount => books.Count;
+
+    public Library(string name, string address)
+    {
+        this.name = name;
+        this.address = address;
+        this.books = new List<string>();
+    }
+
+    public void AddBook(string bookTitle)
+    {
+        books.Add(bookTitle);
+        Console.WriteLine($"Книга \"{bookTitle}\" додана до бібліотеки.");
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Бібліотека: {name}, Адреса: {address}, Книг: {BooksCount}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Library lib = new Library("Центральна бібліотека", "м. Рівне, вул. Соборна, 12");
+        lib.AddBook("Кобзар");
+        lib.AddBook("Місто");
+        lib.PrintInfo();
+    }
+}
