7bff06fb37626ef6f47be29868847d50317c5a74
Максим Поліщук <polishchuk.maksim2308@gmail.com>
2025-12-03 16:35:56 +0200

    Update assembly information and add .gitignore for Visual Studio project

--- MyCSharpProject/obj/Debug/net10.0/MyCSharpProject.AssemblyInfo.cs
+++ MyCSharpProject/obj/Debug/net10.0/MyCSharpProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beabe3e1182e2e1ad2acc27c59db2cdbf9460b3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
